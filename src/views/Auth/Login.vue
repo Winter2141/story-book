c38798054b547faf0b5ed65899a4ecb3c35a0f0a
Athene <56195557+Winter2141@users.noreply.github.com>
2023-08-14 02:42:02 +0800

    Update Login.vue

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -87,12 +87,12 @@
             </div>
 
             <div class="flex items-center justify-center flex-col gap-4 mt-2">
-                <router-link
-                        :to="{ name: 'forget-password' }"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </router-link>
+<!--                <router-link-->
+<!--                        :to="{ name: 'forget-password' }"-->
+<!--                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"-->
+<!--                >-->
+<!--                    Forgot your password?-->
+<!--                </router-link>-->
 
                 <PrimaryButton class="ml-4 w-full justify-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
